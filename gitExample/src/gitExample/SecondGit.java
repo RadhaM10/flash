9620a15ee8a7dfcdc3d01cb50a4a2d503f475395
sasik <sasik@LAPTOP-7MISCB4H>
2020-07-26 17:39:28 -0400

    uploaded all files

---
+++ gitExample/src/gitExample/SecondGit.java
@@ -0,0 +1,10 @@
+package gitExample;
+
+public class SecondGit {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ gitExample/src/gitExample/firstGit.java
@@ -0,0 +1,25 @@
+package gitExample;
+
+
+	
+	class abc{
+
+		int add(int num1,int num2)
+		{
+		int c= num1+num2;
+
+		return c;
+		
+	}
+	}
+		public class firstGit {
+
+		public static void main(String[] args) 
+		{
+			
+			abc a=new abc();
+     
+     System.out.println(a.add());
+
+}
+}
